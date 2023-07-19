574ae5e91bcda09f94b14dd63af98a2efba71c0a
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2023-07-19 09:30:44 -0400

    Aprendendo sobre mark.fail e skipif

--- Pytest/tests/test.py
+++ Pytest/tests/test.py
@@ -4,11 +4,11 @@ from codigo.app import bricadeira
 @mark.skip(reason="pq está falhando")
 def test_quando_bricadeira_recerber_1_entao_retorna_1():
     assert bricadeira(1) == 1
+ 
+ 
+def test_quando_bricadeira_recerber_qualquer_numero_entao_retorna_ele_mesmo():
+    assert bricadeira(2) == 2
     
-def test_quando_bricadeira_recerber_qualquer_numero_entao_retorna_1():
-    assert bricadeira(4) == 2
-    
-    print("Estoy aqui :)")
     
 def test_quando_bricadeira_receber_5_retorna_goiabada():
     
@@ -17,7 +17,7 @@ def test_quando_bricadeira_receber_5_retorna_goiabada():
 @mark.parametrizado 
 @mark.parametrize(
     'entrada',
-    [3,4,20,25,35]
+    [5,10,20,25,35]
 )
 def test__bricdeira_deve_retornar_goiabada_com_multiplos_de_5(entrada):
     
@@ -26,11 +26,11 @@ def test__bricdeira_deve_retornar_goiabada_com_multiplos_de_5(entrada):
 @mark.multiplo3
 @mark.parametrize(
     'entrada',
-    [3,20,9,12,18]
+    [3,6,9,12,18]
 )
 def test__bricdeira_deve_retornar_queijo_com_multiplos_de_3(entrada):
     
-    assert bricadeira(entrada) == 'queijo'
+    assert bricadeira(entrada) == 'queijo' 
     
 @mark.esperado
 @mark.parametrize(
@@ -40,3 +40,8 @@ def test__bricdeira_deve_retornar_queijo_com_multiplos_de_3(entrada):
 def test__bricdeira_deve_retornar_valor_esperado(entrada, esperado):
     
     assert bricadeira(entrada) == esperado
+    
+    
+@mark.xfail    
+def test_xfail():
+    assert bricadeira(20) == 'goiabada'
