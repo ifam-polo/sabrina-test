454eddc09a111215a03c861ad2ce1ca9cdf1b1e9
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2023-07-26 15:55:08 -0400

    fazendo test em views

--- test_Django/recipes/tests/test_recipe_views.py
+++ test_Django/recipes/tests/test_recipe_views.py
@@ -5,7 +5,7 @@ from recipes import views
 
     
 class RecipeViewsTest(TestCase):
-        # verificar se a view da home está sendo renderizada quando acessada pela url
+        # verificar se a class da view da home está sendo renderizada quando acessada pela url
     def test_recipe_home_views_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func.view_class, views.RecipeListViewHome)
@@ -28,7 +28,7 @@ class RecipeViewsTest(TestCase):
             response.content.decode('utf-8'))
         
             
-    # verificar se a view da category está sendo renderizada quando acessada pela url 
+    # verificar se a class view da category está sendo renderizada quando acessada pela url
     def test_recipe_category_view_function_is_correct(self):
        view = resolve(reverse('recipes:category', kwargs={'category_id':1}))
        self.assertIs(view.func.view_class, views.RecipeListViewCategory)   
@@ -39,13 +39,13 @@ class RecipeViewsTest(TestCase):
      #   self.assertEqual(response.status_code, 404)
        
         
-    # verificar se a view da detail está sendo renderizada quando acessada pela url  
+    # verificar se a class view da detail está sendo renderizada quando acessada pela url
     def test_recipe_detail_views_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'pk':1}))
         self.assertIs(view.func.view_class, views.RecipeDetail)
         
         
-    # verificar se a view da search está sendo renderizada quando acessada pela url
+    # verificar se a class view da search está sendo renderizada quando acessada pela url
     def test_recipe_search_views_function_is_correct(self):
         view = resolve(reverse('recipes:search'))
         self.assertIs(view.func.view_class, views.RecipeListViewSearch)
--- test_Django/recipes/tests/test_views.py
+++ test_Django/recipes/tests/test_views.py
@@ -1,6 +1,56 @@
 from django.urls import reverse, resolve
+from recipes import views
+import pytest
 
-# Verificar se a view da home esta sendo renderizada quando acessada pela url 
+
+# Verificar se a view da home esta sendo renderizada quando acessada pela url
+
 def test_recipe_view_home_fuction_is_correct():
-    view = reverse(resolve('recipes:home'))
-    assert 1 == 1
+    view = resolve(reverse('recipes:home'))
+    assert view.func.view_class is views.RecipeListViewHome
+
+#verificar se o status code da view é 200
+@pytest.mark.django_db
+def test_recipe_home_view_returns_status_code_200_ok(client):
+    response = client.get(reverse('recipes:home'))
+    assert response.status_code == 200
+
+#verificar se a view da home está renderizando o template certo
+def test_recipe_home_view_load_correct(client):
+    response = client.get(reverse('recipes:home'))
+    assert response == '/recipes/pages/home.html'
+
+
+#Verificar se a view da category esta sendo renderizada quando acessada pela url
+def test_recipe_view_category_fuction_is_correct():
+    view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+    assert view.func.view_class is views.RecipeListViewCategory
+
+#Verificar se a view da category esta sendo renderizada quando acessada pela url
+def test_recipe_view_datail_fuction_is_correct():
+    view = resolve(reverse('recipes:recipe', kwargs={'pk': 1}))
+    assert view.func.view_class is views.RecipeDetail
+
+#Verificar se a view da detail esta sendo renderizada quando acessada pela url
+
+def test_recipe_view_search_fuction_is_correct():
+    view = resolve(reverse('recipes:search'))
+    assert view.func.view_class is views.RecipeListViewSearch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
